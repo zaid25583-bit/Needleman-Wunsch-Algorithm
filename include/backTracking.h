27caaf2966971d5e309b4a58fba1f8f488b945f6
Zaid Baig <zaid25583@iiitd.ac.in>
2026-08-18 20:04:41 +0530

    Create backTracking.h

---
+++ include/backTracking.h
@@ -0,0 +1,16 @@
+#ifndef BACKTRACKING_H
+#define BACKTRACKING_H
+
+#include <vector>
+#include <string>
+
+int backTrack (
+  const std::vector<std::vector<int>> seqMatrix,
+  const std::string seqOne,
+  const std::string seqTwo, 
+  int match,
+  int pMismatch,
+  int pGap
+);
+
+#endif
